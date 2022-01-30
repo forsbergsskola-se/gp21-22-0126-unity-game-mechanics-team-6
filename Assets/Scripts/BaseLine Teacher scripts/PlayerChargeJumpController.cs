be920933367401cdec524f84cfc31a6c784471c3
Emanoel Kouriat <emanoel.kouriat@student.forsbergsskola.se>
2022-01-30 01:18:47 +0100

    PlayerChargeJumpController.cs: Changed private to public

--- Assets/Scripts/BaseLine Teacher scripts/PlayerChargeJumpController.cs
+++ Assets/Scripts/BaseLine Teacher scripts/PlayerChargeJumpController.cs
@@ -5,8 +5,8 @@ public class PlayerChargeJumpController : MonoBehaviour
     [SerializeField] private Rigidbody myRigidbody;
     [SerializeField] private PlayerInputController playerInputController;
     [SerializeField] private GroundChecker groundChecker;
-    [SerializeField] private float minimumJumpForce = 100f;
-    [SerializeField] private float maximumJumpForce = 1000f;
+    [SerializeField] public float minimumJumpForce = 100f;
+    [SerializeField] public float maximumJumpForce = 1000f;
     [SerializeField] private float chargeTime = 1f;
 
     private float jumpCharge;
