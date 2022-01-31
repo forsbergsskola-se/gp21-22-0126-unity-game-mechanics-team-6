c5eaea141a9017aaae66d800918f12d4fb4aacd8
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-01-31 13:52:48 +0100

    FlightSystem.cs: Base math now in place for triggering flight.

--- Assets/Scripts/Oskar/Movement/Implementation 1/FlightSystem.cs
+++ Assets/Scripts/Oskar/Movement/Implementation 1/FlightSystem.cs
@@ -1,11 +1,83 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
 
 public class FlightSystem : MonoBehaviour
 {
-    public void Fly()
+    public bool Fly;
+
+    [SerializeField] private float maxFlightFuel;
+    [SerializeField] private float fuelUsagePerSecond;
+    [SerializeField] private float fuelRegenPerSecond;
+    [SerializeField] private int maxFuelRegenCooldown;
+
+    private float fuelUsagePerUpdate;
+    private float fuelRegenPerUpdate;
+    private float flightFuel;
+    private int fuelRegenCooldown;
+    
+    
+    private float FlightFuel
     {
+        get => flightFuel;
+        set
+        {
+            flightFuel = Mathf.Clamp(value, 0, maxFlightFuel);
+            Debug.Log($"Fuel level: {flightFuel}");
+        }
+    }
+    
+    
+
+    private void Start()
+    {
+        FlightFuel = maxFlightFuel;
+        fuelRegenPerUpdate = fuelRegenPerSecond / 60;
+        fuelUsagePerUpdate = fuelUsagePerSecond / 60;
+    }
+    
+    
+    
+    private void FixedUpdate()
+    {
+        if (!Fly)
+        {
+            RegenerateFuel();
+            return;
+        }
+
+        if (FlightFuel > fuelUsagePerUpdate)
+        {
+            FlightFuel -= fuelUsagePerUpdate;
+            
+            fuelRegenCooldown = maxFuelRegenCooldown;
+            
+            BoostUpOnce();
+        }
+        else Debug.Log("Out of fuel");
         
+        Fly = false;
+    }
+
+    
+    
+    void BoostUpOnce()
+    {
+        // FlyHere
+    }
+    
+    
+
+    void RegenerateFuel()
+    {
+        if (fuelRegenCooldown > 0)
+        {
+            fuelRegenCooldown--;
+            return;
+        }
+        
+        FlightFuel += fuelRegenPerUpdate;
     }
 }
--- Assets/Scripts/Oskar/Movement/Implementation 1/MovementController.cs
+++ Assets/Scripts/Oskar/Movement/Implementation 1/MovementController.cs
@@ -8,6 +8,6 @@ public class MovementController : MonoBehaviour
 
     public void Fly()
     {
-        flightSystem.Fly();
+        flightSystem.Fly = true;
     }
 }
