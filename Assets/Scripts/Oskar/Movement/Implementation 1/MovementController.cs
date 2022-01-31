33db110ca14716ca205588578b53945bbd06c8b9
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-01-31 14:34:49 +0100

    MovementController.cs: Now support calls to side movement

--- Assets/Scripts/Oskar/Movement/Implementation 1/MovementController.cs
+++ Assets/Scripts/Oskar/Movement/Implementation 1/MovementController.cs
@@ -1,4 +1,5 @@
 using Oskar.Movement.Implementation1.Flight;
+using Oskar.Movement.Implementation1.HorizontalMovement;
 using UnityEngine;
 
 namespace Oskar.Movement.Implementation1.Control
@@ -6,10 +7,12 @@ namespace Oskar.Movement.Implementation1.Control
     public class MovementController : MonoBehaviour
     {
         [SerializeField] private FlightSystem flightSystem;
+        [SerializeField] private HorizontalMovementSystem horizontalMovementSystem;
 
-        public void Fly()
-        {
-            flightSystem.Fly = true;
-        }
+        
+        
+        public void Fly() => flightSystem.Fly = true;
+        public void MoveLeft() => horizontalMovementSystem.MoveLeft = true;
+        public void MoveRight() => horizontalMovementSystem.MoveRight = true;
     }
 }
