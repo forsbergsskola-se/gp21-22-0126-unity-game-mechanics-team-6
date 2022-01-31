ec611be9a8db8286af416958626662c69c585968
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-01-31 14:34:20 +0100

    HorizontalMovementSystem.cs: Added empty script.

---
+++ Assets/Scripts/Oskar/Movement/Implementation 1/HorizontalMovementSystem.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Oskar.Movement.Implementation1.HorizontalMovement
+{
+    public class HorizontalMovementSystem : MonoBehaviour
+    {
+        public bool MoveLeft;
+        public bool MoveRight;
+    }
+}
