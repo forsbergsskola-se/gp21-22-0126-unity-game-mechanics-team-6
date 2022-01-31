26ed17a97b5a1be0a66114361a80dfd8e86fc112
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-01-31 14:26:11 +0100

    KeyBinds.cs: Now uses correct namespace.

--- Assets/Scripts/Oskar/Movement/Implementation 1/KeyBinds.cs
+++ Assets/Scripts/Oskar/Movement/Implementation 1/KeyBinds.cs
@@ -1,10 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class KeyBinds : MonoBehaviour
+namespace Oskar.Movement.Implementation1.Control
 {
-    public KeyCode MoveLeft = KeyCode.A;
-    public KeyCode MoveRight = KeyCode.D;
-    public KeyCode FlyUp = KeyCode.W;
+    public class KeyBinds : MonoBehaviour
+    {
+        public KeyCode MoveLeft = KeyCode.A;
+        public KeyCode MoveRight = KeyCode.D;
+        public KeyCode FlyUp = KeyCode.W;
+    }
 }
